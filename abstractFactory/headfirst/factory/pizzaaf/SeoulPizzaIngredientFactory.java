3bc3cc72a373428f30cda7440106b19e3ab43e08
bill291104 <bill29123@gmail.com>
2024-11-16 19:10:24 +0900

    feat(SeoulPizzaIngredientFactory): make SeoulPizzaIngredientFactory implementing PizzaIngredientFactory
    - Seoul pizza ingredient factory supply rice dough, plum tomato sauce, cheddar cheese, veggies, sliced pepperoni and pen shell

---
+++ abstractFactory/headfirst/factory/pizzaaf/SeoulPizzaIngredientFactory.java
@@ -0,0 +1,40 @@
+package headfirst.factory.pizzaaf;
+
+public class SeoulPizzaIngredientFactory implements PizzaIngredientFactory {
+    @Override
+    public Dough createDough() {
+        return new RiceDough();
+    }
+
+    @Override
+    public Sauce createSauce() {
+        return new PlumTomatoSauce();
+    }
+
+    @Override
+    public Cheese createCheese() {
+        return new CheddarCheese();
+    }
+
+    @Override
+    public Veggies[] createVeggies() {
+        Veggies veggies[] = { new Garlic(),
+                new Onion(),
+                new Mushroom(),
+                new RedPepper(),
+                new BlackOlives(),
+                new Spinach(),
+                new Eggplant()};
+        return veggies;
+    }
+
+    @Override
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    }
+
+    @Override
+    public Clams createClam() {
+        return new PenShell();
+    }
+}
