23029691b9ad41ed4e2a2f866c72b745d9f1537f
bill291104 <bill29123@gmail.com>
2024-11-16 18:53:31 +0900

    fix(PizzaTestDrive): add Seoul pizza store
    - New pizza store opened in Seoul

--- abstractFactory/headfirst/factory/pizzaaf/PizzaTestDrive.java
+++ abstractFactory/headfirst/factory/pizzaaf/PizzaTestDrive.java
@@ -5,6 +5,7 @@ public class PizzaTestDrive {
 	public static void main(String[] args) {
 		PizzaStore nyStore = new NYPizzaStore();
 		PizzaStore chicagoStore = new ChicagoPizzaStore();
+		PizzaStore seoulStore = new SeoulPizzaStore();
  
 		Pizza pizza = nyStore.orderPizza("cheese");
 		System.out.println("Ethan ordered a " + pizza + "\n");
