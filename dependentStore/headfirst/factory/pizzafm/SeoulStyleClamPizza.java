a28318c36d488fdba25ebe99ac831a88f22e0cf3
bill291104 <bill29123@gmail.com>
2024-11-16 18:29:54 +0900

    fix(SeoulPizzaStore): fix bug
    - Fix createPizza method in SeoulPizzaStore
    - Create correct type of pizza

--- dependentStore/headfirst/factory/pizzafm/SeoulStyleClamPizza.java
+++ dependentStore/headfirst/factory/pizzafm/SeoulStyleClamPizza.java
@@ -2,7 +2,7 @@ package headfirst.factory.pizzafm;
 
 public class SeoulStyleClamPizza extends Pizza {
     public SeoulStyleClamPizza() {
-        name = "Seoul Style Cheese Pizza";
+        name = "Seoul Style Clam Pizza";
         dough = "Normal Dough";
         sauce = "Normal Tomato Sauce";
 
